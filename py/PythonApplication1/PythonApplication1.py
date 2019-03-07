f234db90cb3278dd851437741408d1051ac23c59
cizlota <cizlota@yandex.ru>
2019-03-07 19:19:34 +0300

    2 buttons

--- py/PythonApplication1/PythonApplication1.py
+++ py/PythonApplication1/PythonApplication1.py
@@ -1,9 +1,16 @@
 
 from tkinter import * 
 
+def fnb(*args):
+    print("a", *args)
+
 root = Tk()
 
-bt = Button(root, text = "1234567")
+bt = Button(root, command = fnb, text = "command")
 bt.grid()
-
+bt2 = Button(root, text = "bind")
+bt2.grid()
+bt2.bind('<Button 1>', fnb)
 root.mainloop()
+
+print("A")
