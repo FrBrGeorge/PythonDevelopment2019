383f9b976baeadc8c844d4200e314668620a2ec0
George V. Kouryachy (Fr. Br. George) <george@altlinux.ru>
2019-03-14 15:29:13 +0300

    Действие по умолчанию для кнопки
    Обратите внимание на механизм обратного вызова:
    функция dump() вызывается не в основном коде программы,
    а при нажатии на кнопку.

--- example03.py
+++ example03.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 '''
 Пример для первой лекции про TkInter
+
+Действие по умолчанию для кнопки (очевидно, клик)
 '''
 
 from tkinter import *
 
+def dump(*args):
+    print("DUMP:",args)
+
 root = Tk()
 root.title("Hello")
+
+Butt = Button(root, text="Butt ON", command=dump)
+Butt.grid()
 root.mainloop()
 
