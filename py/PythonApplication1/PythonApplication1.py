9f0a2a137e11ab4d7ed4b92ad56baaa785f40995
cizlota <cizlota@yandex.ru>
2019-03-14 19:15:08 +0300

    Пользуемся гридом

--- py/PythonApplication1/PythonApplication1.py
+++ py/PythonApplication1/PythonApplication1.py
@@ -11,24 +11,30 @@ def move(*args):
         time.sleep(0.1)
 
 root = Tk()
-
+root.title("=)")
+root.columnconfigure(0, weight = 1)
+root.columnconfigure(1, weight = 2)
+root.rowconfigure(0, weight = 1)
+root.rowconfigure(1, weight = 3)
+root.rowconfigure(2, weight = 1)
 bt = Button(root, command = fnb, text = "   command                          ")
-bt.pack()
+bt.grid(row = 0, column = 0)
+
 bt2 = Button(root, text = "               bind                     ")
-bt2.pack(side = TOP, fill=X)
+bt2.grid(row = 0, column = 1)
 bt2.bind('<Button 1>', fnb)
-bt3 = Button(root, text = "              move                ")
+
+bt3 = Button(root, text = "              move                ", bg = "darkRED", fg = "peachpuff")
 bt3.bind('<Button 1>', move)
-bt3.pack(side  = LEFT)
+bt3.grid(row = 1, column = 0, sticky = W + E + N + S, columnspan = 2)
+
 btq = Button(root, command = root.destroy, text = "           quit                           ")
-btq.pack()
+btq.grid(row = 2, column = 1)
 
 txt = Label(root, text="LABELLABELLABELLABELLABEL")
 txt.configure(bg = "black", fg = "white")
-txt.place(x = 10, y = 10)
 
-
-#txt.grid
+#txt.grid()
 
 root.mainloop()
 
