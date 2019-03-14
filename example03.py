64cbff1085969dcec3c82e7b341f63084249e4b3
George V. Kouryachy (Fr. Br. George) <george@altlinux.ru>
2019-03-14 19:14:31 +0300

    Изменяемые размеры ячеек .grid()

--- example03.py
+++ example03.py
@@ -13,13 +13,17 @@ def dump(*args):
 root = Tk()
 root.title("Hello")
 
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=2)
+root.rowconfigure(0, weight=1)
+
 Butt = Button(root, text="Butt ON")
 Butt.bind('<Button-1>', dump)
-Butt.grid(row=0, column=0)
+Butt.grid(row=0, column=0, sticky=E+W)
 Exit = Button(root, text="Quit!", command=root.quit)
-Exit.grid(row=0, column=1)
+Exit.grid(row=0, column=1, sticky=E+W)
 Txt = Label(root, text="This is a label", bg="PeachPuff")
-Txt.grid(row=1, column=0, columnspan=2)
+Txt.grid(row=1, column=0, columnspan=2, sticky=E+W)
 
 root.mainloop()
 print("Done")
