b6e524d84d7df0d932a901ac766e2b8c2851c681
cizlota <cizlota@yandex.ru>
2019-03-14 18:56:52 +0300

    побаловался с жеометри менеджерами

--- py/PythonApplication1/PythonApplication1.py
+++ py/PythonApplication1/PythonApplication1.py
@@ -1,19 +1,35 @@
 
 from tkinter import * 
+import time
 
 def fnb(*args):
     print("a", *args)
 
+def move(*args):
+    for i in range (100):
+        txt.place(x=i,y=i)
+        time.sleep(0.1)
+
 root = Tk()
 
-bt = Button(root, command = fnb, text = "command")
-bt.grid()
-bt2 = Button(root, text = "bind")
-bt2.grid()
+bt = Button(root, command = fnb, text = "   command                          ")
+bt.pack()
+bt2 = Button(root, text = "               bind                     ")
+bt2.pack(side = TOP, fill=X)
 bt2.bind('<Button 1>', fnb)
+bt3 = Button(root, text = "              move                ")
+bt3.bind('<Button 1>', move)
+bt3.pack(side  = LEFT)
+btq = Button(root, command = root.destroy, text = "           quit                           ")
+btq.pack()
 
-btq = Button(root, command = root.destroy, text = "quit")
-btq.grid()
+txt = Label(root, text="LABELLABELLABELLABELLABEL")
+txt.configure(bg = "black", fg = "white")
+txt.place(x = 10, y = 10)
+
+
+#txt.grid
+
 root.mainloop()
 
 print("A")
