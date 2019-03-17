faa87238fd6b4a34a256461423c3bae29ee50db9
cizlota <cizlota@yandex.ru>
2019-03-17 21:40:53 +0300

    Начало ДЗ

--- py/PythonApplication1/PythonApplication1.py
+++ py/PythonApplication1/PythonApplication1.py
@@ -1,14 +1,14 @@
 
 from tkinter import * 
 import time
+import random 
 
-def fnb(*args):
-    print("a", *args)
+def bt_func(*args):
+    bt3.grid(row = 1, column = 0)
+    txt.grid(row = 1, column = 1)
 
-def move(*args):
-    for i in range (100):
-        txt.place(x=i,y=i)
-        time.sleep(0.1)
+def bt3_func(*args):
+    txt.configure(bg = "#%06x" % random.randint(0, 0xFFFFFF), fg = "#%06x" % random.randint(0, 0xFFFFFF))
 
 TKroot = Tk()
 TKroot.title("=)")
@@ -20,22 +20,20 @@ root.columnconfigure(1, weight = 2)
 root.rowconfigure(0, weight = 1)
 root.rowconfigure(1, weight = 3)
 root.rowconfigure(2, weight = 1)
-bt = Button(root, command = fnb, text = "   command                          ")
+
+bt = Button(root, command = bt_func, text = "ADD")
 bt.grid(row = 0, column = 0)
 
-bt2 = Button(root, text = "               bind                     ")
-bt2.grid(row = 0, column = 1)
-bt2.bind('<Button 1>', fnb)
+bt_exit= Button(root, text = "EXIT", command = TKroot.destroy)
+bt_exit.grid(row = 0, column = 1)
 
-bt3 = Button(root, text = "              move                ", bg = "darkRED", fg = "peachpuff")
-bt3.bind('<Button 1>', move)
-bt3.grid(row = 1, column = 0, sticky = W + E + N + S, columnspan = 2)
 
-btq = Button(root, command = root.destroy, text = "           quit                           ")
-btq.grid(row = 2, column = 1)
+bt3 = Button(root, text = "BUTTON", bg = "darkRED", fg = "peachpuff")
+bt3.bind('<Button 1>', bt3_func)
 
 txt = Label(root, text="LABELLABELLABELLABELLABEL")
-txt.configure(bg = "black", fg = "white")
+
+
 
 #txt.grid()
 
