19b8b728d4bbb52b154e027af64224b33114f036
cizlota <cizlota@yandex.ru>
2019-03-17 21:50:11 +0300

    ДЗ 3(Кнопки и метка по разному растягиваются)

--- py/PythonApplication1/PythonApplication1.py
+++ py/PythonApplication1/PythonApplication1.py
@@ -4,8 +4,8 @@ import time
 import random 
 
 def bt_func(*args):
-    bt3.grid(row = 1, column = 0)
-    txt.grid(row = 1, column = 1)
+    bt3.grid(row = 1, column = 0, sticky = W + E + N + S)
+    txt.grid(row = 1, column = 1, sticky = W + E + N + S)
 
 def bt3_func(*args):
     txt.configure(bg = "#%06x" % random.randint(0, 0xFFFFFF), fg = "#%06x" % random.randint(0, 0xFFFFFF))
@@ -19,13 +19,12 @@ root.columnconfigure(0, weight = 1)
 root.columnconfigure(1, weight = 2)
 root.rowconfigure(0, weight = 1)
 root.rowconfigure(1, weight = 3)
-root.rowconfigure(2, weight = 1)
 
 bt = Button(root, command = bt_func, text = "ADD")
-bt.grid(row = 0, column = 0)
+bt.grid(row = 0, column = 0, sticky = W + E)
 
 bt_exit= Button(root, text = "EXIT", command = TKroot.destroy)
-bt_exit.grid(row = 0, column = 1)
+bt_exit.grid(row = 0, column = 1, sticky = W + E)
 
 
 bt3 = Button(root, text = "BUTTON", bg = "darkRED", fg = "peachpuff")
@@ -34,9 +33,5 @@ bt3.bind('<Button 1>', bt3_func)
 txt = Label(root, text="LABELLABELLABELLABELLABEL")
 
 
-
-#txt.grid()
-
 root.mainloop()
 
-print("A")
