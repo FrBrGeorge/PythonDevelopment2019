547c89470086856675d484246362b2cdc61438c9
cizlota <cizlota@yandex.ru>
2019-03-21 18:46:02 +0300

    Из первого ентри во второй по кнопке

--- py/PythonApplication1/test2.py
+++ py/PythonApplication1/test2.py
@@ -9,23 +9,23 @@ def GetText(*args):
     print(txt.get())
 
 def PutText(*args):
-    pass
+    t = txt.get()
+    txt2.delete(0, END)
+    txt2.insert(0, t)
 
 TKroot = Tk()
 TKroot.title("=)")
 root = Frame(TKroot)
 root.place(relheight = 1.0, relwidth = 1.0)
 
-
-
 txt = Entry(root, text = "TEXT")
 txt.grid(columnspan =2)
 
-txt2 = Entry(root, text = "TEXT")
+txt2 = Entry(root, text = "TEXT2")
 txt2.grid(columnspan =2)
 
-get = Button(root, text = "get", command =GetText)
-get.grid();
+getBT = Button(root, text = "get", command =GetText)
+getBT.grid();
 
 put = Button(root, text = "put", command = PutText)
 put.grid(column  = 1, row = 2);
