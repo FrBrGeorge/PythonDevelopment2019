c9893371ee0d6afd9a84c2c5b4c330d3dc191bf0
George V. Kouryachy (Fr. Br. George) <george@altlinux.ru>
2019-03-21 16:00:42 +0300

    Add is now adds Button/Label
    04_PublishingAndGeometry part 1 (incomplete) and 3

--- example03.py
+++ example03.py
@@ -9,6 +9,15 @@ def dump(*args, **argp):
     '''Prints any of arguments passed'''
     print("DUMP:",args,argp)
 
+def addadd():
+    '''Add new Button/label pair to root'''
+    b = Button(root, text="Color")
+    l = Label(root, text="Color")
+    c, r = root.size()
+    root.rowconfigure(r+1, weight=1)
+    b.grid(row=r+1, column=0, sticky=E+W+S+N)
+    l.grid(row=r+1, column=1, sticky=E+W+S+N)
+
 TKroot = Tk()
 TKroot.title("Hello")
 
@@ -20,10 +29,8 @@ root.grid(sticky=E+W+S+N)
 root.columnconfigure(0, weight=1)
 root.columnconfigure(1, weight=1)
 root.rowconfigure(0, weight=0)
-root.rowconfigure(1, weight=1)
 
-Add = Button(root, text="Add")
-Add.bind('<Button-1>', dump)
+Add = Button(root, text="Add", command=addadd)
 Add.grid(row=0, column=0, sticky=E+W+S+N)
 Exit = Button(root, text="Exit", command=root.quit)
 Exit.grid(row=0, column=1, sticky=E+W+S+N)
