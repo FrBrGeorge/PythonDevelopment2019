cf1e1c93d3e44f7623d1c8ce2ba42ab9fce094b2
cizlota <cizlota@yandex.ru>
2019-03-21 18:41:41 +0300

    Подготовка к чему-то большому?

--- py/PythonApplication1/test2.py
+++ py/PythonApplication1/test2.py
@@ -3,25 +3,33 @@
 Работа с textvariable
 '''
 
-from tkinter import * 
+from tkinter import *
+
+def GetText(*args):
+    print(txt.get())
+
+def PutText(*args):
+    pass
 
 TKroot = Tk()
 TKroot.title("=)")
 root = Frame(TKroot)
 root.place(relheight = 1.0, relwidth = 1.0)
 
-root.columnconfigure(0, weight = 1)
-root.columnconfigure(1, weight = 2)
-root.rowconfigure(0, weight = 1)
-root.rowconfigure(1, weight = 3)
+
 
 txt = Entry(root, text = "TEXT")
-txt.grid()
-def GetText(*args):
-    print(txt.get())
+txt.grid(columnspan =2)
 
+txt2 = Entry(root, text = "TEXT")
+txt2.grid(columnspan =2)
 
 get = Button(root, text = "get", command =GetText)
 get.grid();
+
+put = Button(root, text = "put", command = PutText)
+put.grid(column  = 1, row = 2);
+
+
 
 TKroot.mainloop()
