5adf7ea15fb5941d8b682179c8ec9ac87bb29114
George V. Kouryachy (Fr. Br. George) <george@altlinux.ru>
2019-03-26 17:49:43 +0300

    Starter example done

--- 05_homework.py
+++ 05_homework.py
@@ -14,19 +14,18 @@ root.columnconfigure(0, weight=0)
 root.columnconfigure(1, weight=1)
 
 def FaceSelect(*args):
-    I["text"]=L.selection_get()
+    I["image"]=Images[L.selection_get()]
 
-Names = {"FrBrGeorge": "Fr. Br. George", "FrBrGeorge_2": "FrBrGeorge avatar"}
-Ids = tuple(Names.keys())
-Nicks = tuple(Names.values())
-Name = StringVar(value=Nicks)
-Id = StringVar(value=Nicks)
+Names = "FrBrGeorge", "FrBrGeorge_2"
+Images = {k:PhotoImage(file=k+".png") for k in Names}
+Name = StringVar(value=Names)
 
 L = Listbox(root, listvariable=Name)
-L.grid(column=0, row=0, sticky=E+W)
+L.grid(column=0, row=0, sticky=E+W+N)
 L.bind('<<ListboxSelect>>', FaceSelect)
 L.selection_set(0)
 I = Label(root)
 I.grid(row=0, column=1, sticky=E+W+S+N)
+FaceSelect()
 
 TKRoot.mainloop()
