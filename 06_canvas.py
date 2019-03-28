2312b74d808c2ac61c1a3e90cf70a48d0254e93b
George V. Kouryachy (Fr. Br. George) <george@altlinux.ru>
2019-03-28 15:45:29 +0300

    Примитивная документация

--- 06_canvas.py
+++ 06_canvas.py
@@ -6,6 +6,7 @@
 from tkinter import *
 
 class App(Frame):
+    '''Base framed application class'''
     def __init__(self, master=None, Title="Application"):
         Frame.__init__(self, master)
         self.master.rowconfigure(0, weight=1)
@@ -16,25 +17,31 @@ class App(Frame):
         self.adjust()
 
     def create(self):
+        '''Create all the widgets'''
         self.bQuit = Button(self, text='Quit', command=self.quit)
         self.bQuit.grid()
 
     def adjust(self):
-        # TODO Smart detecting resizeable/still/wide cells
+        '''Adjust grid sise/properties'''
+        # TODO Smart detecting resizeable/still cells
         for i in range(self.size()[0]):
             self.columnconfigure(i, weight=12)
         for i in range(self.size()[1]):
             self.rowconfigure(i, weight=12)
         
 class Paint(Canvas):
+    '''Canvas with simple drawing'''
     def mousedown(self, event):
+        '''Store mousedown coords'''
         self.x0, self.y0 = event.x, event.y
 
     def mousemove(self, event):
+        '''Do sometheing when drag a mouse'''
         l=self.create_line((self.x0, self.y0, event.x, event.y))
         print(l)
 
     def mouseup(self, event):
+        '''Dragging is done'''
         print(self.find_all())
 
     def __init__(self, master=None):
