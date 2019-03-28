101f6883bd122fbae096e594ec3ea051cd272a8b
George V. Kouryachy (Fr. Br. George) <george@altlinux.ru>
2019-03-28 14:14:26 +0300

    Затравка для Canvas

--- 06_canvas.py
+++ 06_canvas.py
@@ -18,6 +18,18 @@ class App(Frame):
         self.bQuit = Button(self, text='Quit', command=self.quit)
         self.bQuit.grid()
         
+class Paint(Canvas):
+    def buttondown(*args):
+        print(*args)
 
-app = App(Title="Example")
+    def __init__(self, master=None):
+        Canvas.__init__(self, master)
+        self.bind("<Button-1>", self.buttondown)
+
+class MyApp(App):
+    def create(self):
+        self.Canvas = Paint(self)
+        self.Canvas.grid(row=0, column=0, sticky=N+E+S+W)
+
+app = MyApp(Title="Canvas Example")
 app.mainloop()
