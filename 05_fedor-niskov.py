1005966dfae56b230d6247bcd51cfb01acfe56c0
Fedor Nis'kov <fniskov@gmail.com>
2019-03-31 00:45:53 +0300

    Added support of TXT-aliases in my program

--- 05_fedor-niskov.py
+++ 05_fedor-niskov.py
@@ -5,6 +5,7 @@
 
 from tkinter import *
 import os
+import sys
 
 TKRoot = Tk()
 TKRoot.columnconfigure(0, weight=1)
@@ -15,15 +16,33 @@ root.columnconfigure(0, weight=0)
 root.columnconfigure(1, weight=1)
 
 def FaceSelect(*args):
-    I["image"]=Images[L.selection_get()]
+    I["image"]=Images[OrigName[L.selection_get()]]
 
 Names = set()
-for n in os.listdir("."):
+Alias = dict()
+OrigName = dict()
+Files = os.listdir(".")
+for n in Files:
     if n.endswith(".png"):
-        Names.add(n[:-4])
-Names = tuple(Names)
+        n0 = n[:-4]
+        Names.add(n0)
+        a = n0
+        nt = n0 + ".txt"
+        if nt in Files:
+            with open(nt) as f:
+                a = f.read().strip()
+        else:
+            print("WARNING: not found " + nt)
+        if a in Alias.values():
+            print("ERROR: " + n0 + ": alias " + a +
+                  " already used for " + OrigName[a])
+            sys.exit(1)
+        Alias[n0] = a
+        OrigName[a] = n0
+
+Aliases = tuple(Alias.values())
 Images = {k:PhotoImage(file=k+".png") for k in Names}
-Name = StringVar(value=Names)
+Name = StringVar(value=Aliases)
 
 L = Listbox(root, listvariable=Name)
 L.grid(column=0, row=0, sticky=E+W+N)
