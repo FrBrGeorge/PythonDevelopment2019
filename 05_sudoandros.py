f3a43e9e039f7c8055cdc67fd31869f3b87f1da8
Andrey Verigin <andrei-verigin@mail.ru>
2019-03-30 22:29:36 +0300

    Add search for images and their names in directory

--- 05_sudoandros.py
+++ 05_sudoandros.py
@@ -4,6 +4,7 @@
 '''
 
 from tkinter import *
+from pathlib import Path
 
 TKRoot = Tk()
 TKRoot.columnconfigure(0, weight=1)
@@ -16,9 +17,21 @@ root.columnconfigure(1, weight=1)
 def FaceSelect(*args):
     I["image"]=Images[L.selection_get()]
 
-Names = "FrBrGeorge", "FrBrGeorge_2"
-Images = {k:PhotoImage(file=k+".png") for k in Names}
-Name = StringVar(value=Names)
+def get_image_name(image_path):
+    txt_path = image_path.parent / (image_path.stem + ".txt")
+    if txt_path.is_file():
+        with txt_path.open("r") as file:
+            return file.read()
+    else:
+        return image_path.stem
+
+Images = {}
+dir_path = Path(__file__).parent
+for file_path in dir_path.iterdir():
+    if file_path.suffix == ".png":
+        image_name = get_image_name(file_path)
+        Images[image_name] = PhotoImage(file=file_path.name)
+Name = StringVar(value=list(Images))
 
 L = Listbox(root, listvariable=Name)
 L.grid(column=0, row=0, sticky=E+W+N)
