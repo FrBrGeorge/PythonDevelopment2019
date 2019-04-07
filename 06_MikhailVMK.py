4bd77e3dd5b0687a9601d5d826850feffbbbb15d
mikhail <mikhailsporyshev@yandex.ru>
2019-04-07 16:48:47 +0300

    color now is the attribute of ToolPanel, ShowColor is an entry, validation function added

--- 06_MikhailVMK.py
+++ 06_MikhailVMK.py
@@ -5,8 +5,63 @@
 from collections import namedtuple
 PanelCoords = namedtuple("PanelCoords", ["row", "column", "rowspan", "columnspan"])
 
+from _tkinter import TclError
 from tkinter import *
 from tkinter import colorchooser
+
+from RgbNamedColors import Colors
+
+def encodedColorValid(encodedColor):
+    if not isinstance(encodedColor, str):
+        return False
+    if encodedColor in Colors.keys():
+        return True
+    if len(encodedColor) != 7:
+        return False
+    if encodedColor[0] != "#":
+        return False
+    for char in encodedColor[1:]:
+        if not char.isdigit() and not char in "abcdefABCDEF":
+            return False
+    return True
+
+def decodedColorValid(decodedColor):
+    if not isinstance(decodedColor, tuple):
+        return False
+    if len(decodedColor) != 3:
+        return False
+    for color in decodedColor:
+        if not isinstance(color, int):
+            return False
+        if color < 0 or color > 255:
+            return False
+    return True             
+
+def getContrastColor(decodedColor):
+    assert decodedColorValid(decodedColor)
+    return ((decodedColor[0] + 128) % 256, (decodedColor[1] + 128) % 256, (decodedColor[2] + 128) % 256)
+
+def encodeColor(decodedColor):
+    assert decodedColorValid(decodedColor)
+    colorString = "#"
+    for i in range(0, len(decodedColor)):
+        color = format(decodedColor[i], 'x')
+        if len(color) == 1:
+            colorString += '0'
+        colorString += color    
+    return colorString
+
+def decodeColor(encodedColor):
+    assert encodedColorValid(encodedColor)
+    if encodedColor in Colors.keys():
+        r = int(Colors[encodedColor][0])
+        g = int(Colors[encodedColor][1])
+        b = int(Colors[encodedColor][2])  
+    else:      
+        r = int(encodedColor[1:3], 16)
+        g = int(encodedColor[3:5], 16)
+        b = int(encodedColor[5:7], 16)
+    return r, g, b
 
 class App(Frame):
     '''Base framed application class'''
@@ -50,9 +105,7 @@ class CanvasPanel(Canvas):
         self.cursor = None
         #print(self.find_all())
 
-    def __init__(self, master=None, *ap, foreground="black", **an):
-        self.foreground = StringVar()
-        self.foreground.set(foreground)
+    def __init__(self, master=None, *ap, **an):
         Canvas.__init__(self, master, *ap, **an)
         self.bind("<Button-1>", self._mousedown)
         self.bind("<B1-Motion>", self._mousemove)
@@ -61,14 +114,14 @@ class CanvasPanel(Canvas):
 class WorkSpace(App):
     def _create(self):
         self._canvasPanelCoords = PanelCoords(row=0, column=0, rowspan = 3, columnspan = 1)
-        self._canvasPanel = CanvasPanel(self, foreground="midnightblue")
+        self._canvasPanel = CanvasPanel(self)
         self._canvasPanel.grid(row=self._canvasPanelCoords.row,
                             column=self._canvasPanelCoords.column,
                             rowspan=self._canvasPanelCoords.rowspan,
                             columnspan=self._canvasPanelCoords.columnspan,
                             sticky=N+E+S+W) 
         self._canvasToolsCoords = PanelCoords(row=0, column=1, rowspan = 3, columnspan = 1) 
-        self._canvasTools = CanvasToolPanel(self, self._canvasPanel)
+        self._canvasTools = CanvasToolPanel(self)
         self._canvasTools.grid(row=self._canvasToolsCoords.row,
                             column=self._canvasToolsCoords.column,
                             rowspan=self._canvasToolsCoords.rowspan,
@@ -85,23 +138,32 @@ class WorkSpace(App):
             print(*self._canvasPanel.coords(item), self._canvasPanel.itemcget(item, "fill"))
 
 class CanvasToolPanel(Frame):
-    def __init__(self, root, canvasPanel):
+    def __init__(self, root, color='black'):
         Frame.__init__(self, root)
         self['borderwidth'] = 2
         self['relief'] = 'raised'
-        self._canvasPanel = canvasPanel
+        self._toolColor = StringVar()
+        self._toolColor.set(color)
         self._askColor = Button(self, text="Color", command=self._askcolor)
         self._askColor.grid(row=0, column=0, sticky=N+W)
-        self._showColor = Label(self, textvariable=self._canvasPanel.foreground, background = self._canvasPanel.foreground.get())
+        self._showColor = Entry(self,
+                            textvariable=self._toolColor,
+                            background =self._toolColor.get(),
+                            foreground = encodeColor(getContrastColor(decodeColor(color))),
+                            validatecommand = (self.register(decodedColorValid), '%P'),
+                            validate='key')
         self._showColor.grid(row=1, column=0, sticky=N+W+E)
         self._quit = Button(self, text="Quit", command=root.quit)
         self._quit.grid(row=2, column=0, sticky=N+W)
 
     def _askcolor(self):
-        self._canvasPanel.foreground.set(colorchooser.askcolor()[1])
-        self._showColor.config(background = self._canvasPanel.foreground.get())
+        color = colorchooser.askcolor(color = self._toolColor.get())[1]
+        self._toolColor.set(color)
+        self._showColor.config(background = color)
+        self._showColor.config(foreground = encodeColor(getContrastColor(decodeColor(color))))
+                
+        
 
 app = WorkSpace(Title="Canvas Example")
 app.mainloop()
 app.printCanvasObjects()
-
