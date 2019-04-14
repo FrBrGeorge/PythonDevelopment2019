2332a58efec805678cf9811369888e79e954063b
snusmumrik <andarpolis@gmail.com>
2019-04-15 01:48:34 +0300

    move all widgets to one class

--- 06_snu5mumr1k.py
+++ 06_snu5mumr1k.py
@@ -9,33 +9,17 @@ from tkinter import *
 from tkinter import colorchooser
 
 
-class App(Frame):
-    """Base framed application class"""
-    def __init__(self, master=None, Title="Application"):
-        Frame.__init__(self, master)
-        self.master.rowconfigure(0, weight=1)
-        self.master.columnconfigure(0, weight=1)
-        self.master.title(Title)
-        self.grid(sticky=N+E+S+W)
-        self.create()
-        self.adjust()
-
-    def create(self):
-        """Create all the widgets"""
-        self.bQuit = Button(self, text="Quit", command=self.quit)
-        self.bQuit.grid()
-
-    def adjust(self):
-        """Adjust grid sise/properties"""
-        # TODO Smart detecting resizeable/still cells
-        for i in range(self.size()[0]):
-            self.columnconfigure(i, weight=12)
-        for i in range(self.size()[1]):
-            self.rowconfigure(i, weight=12)
-
-
 class Paint(Canvas):
     """Canvas with simple drawing"""
+    def __init__(self, master=None, *args, foreground="black", **kwargs):
+        self.foreground = StringVar()
+        self.foreground.set(foreground)
+
+        super().__init__(master=master, *args, **kwargs)
+        self.bind("<Button-1>", self.mousedown)
+        self.bind("<B1-Motion>", self.mousemove)
+        self.bind("<ButtonRelease-1>", self.mouseup)
+
     def mousedown(self, event):
         """Store mousedown coords"""
         self.x0, self.y0 = event.x, event.y
@@ -51,20 +35,19 @@ class Paint(Canvas):
         """Dragging is done"""
         self.cursor = None
 
-    def __init__(self, master=None, *ap, foreground="black", **an):
-        self.foreground = StringVar()
-        self.foreground.set(foreground)
-        Canvas.__init__(self, master, *ap, **an)
-        self.bind("<Button-1>", self.mousedown)
-        self.bind("<B1-Motion>", self.mousemove)
-        self.bind("<ButtonRelease-1>", self.mouseup)
 
+class Application(Frame):
+    def __init__(self, master=None, Title="Application"):
+        super().__init__(master=master)
 
-class MyApp(App):
-    def askcolor(self):
-        self.Canvas.foreground.set(colorchooser.askcolor()[1])
+        self.master.rowconfigure(0, weight=1)
+        self.master.columnconfigure(0, weight=1)
+        self.master.title(Title)
+        self.grid(sticky=N+E+S+W)
+        self.create_widgets()
+        self.adjust()
 
-    def create(self):
+    def create_widgets(self):
         self.Canvas = Paint(self, foreground="midnightblue")
         self.Canvas.grid(row=0, column=0, rowspan=3, sticky=N+E+S+W)
         self.AskColor = Button(self, text="Color", command=self.askcolor)
@@ -74,12 +57,21 @@ class MyApp(App):
         self.Quit = Button(self, text="Quit", command=self.quit)
         self.Quit.grid(row=2, column=1, sticky=N+W)
 
+    def adjust(self):
+        """Adjust grid size/properties"""
+        # TODO Smart detecting resizeable/still cells
+        for i in range(self.size()[0]):
+            self.columnconfigure(i, weight=12)
+        for i in range(self.size()[1]):
+            self.rowconfigure(i, weight=12)
+
+    def askcolor(self):
+        self.Canvas.foreground.set(colorchooser.askcolor()[1])
+
 
 def main():
-    app = MyApp(Title="Canvas Example")
-    app.mainloop()
-    for item in app.Canvas.find_all():
-        print(*app.Canvas.coords(item), app.Canvas.itemcget(item, "fill"))
+    application = Application(Title="Canvas Example")
+    application.mainloop()
 
 
 if __name__ == "__main__":
