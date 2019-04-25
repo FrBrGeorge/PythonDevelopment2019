720201cad3b5670cc2870790f9bcff705aff50a3
George V. Kouryachy (Fr. Br. George) <george@altlinux.ru>
2019-04-25 15:50:23 +0300

    U14n done

--- 10_Translation/app.py
+++ 10_Translation/app.py
@@ -3,6 +3,7 @@
 from tkinter import *
 import sys
 import os.path
+import gettext
 
 def calculate(*args):
     try:
@@ -11,10 +12,11 @@ def calculate(*args):
     except ValueError:
         pass
 
-imgpath = os.path.dirname(sys.argv[0])
+datapath = os.path.dirname(sys.argv[0])
+gettext.install('messages', datapath)
 
 root = Tk()
-root.title("Feet to Meters")
+root.title(_("Feet to Meters"))
 
 mainframe = Frame(root, padx=3, pady=12)
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
@@ -24,19 +26,19 @@ root.rowconfigure(0, weight=1)
 feet = StringVar()
 meters = StringVar()
 
-Logo = PhotoImage(file=os.path.join(imgpath,"LogoEN.png"))
+Logo = PhotoImage(file=os.path.join(datapath,_("LogoEN.png")))
 Label(mainframe, image=Logo).grid(column=1, row=1, sticky=(N, W))
 
 feet_entry = Entry(mainframe, width=7, textvariable=feet)
 feet_entry.grid(column=2, row=1, sticky=(W, E))
 
 Label(mainframe, textvariable=meters, relief=GROOVE).grid(column=2, row=2, sticky=(W, E))
-Button(mainframe, text="Calculate", command=calculate)\
+Button(mainframe, text=_("Calculate"), command=calculate)\
     .grid(column=3, row=3, sticky=W)
 
-Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+Label(mainframe, text=_("feet")).grid(column=3, row=1, sticky=W)
+Label(mainframe, text=_("is equivalent to")).grid(column=1, row=2, sticky=E)
+Label(mainframe, text=_("meters")).grid(column=3, row=2, sticky=W)
 
 for child in mainframe.winfo_children():
     child.grid_configure(padx=5, pady=5)
